08d8b9456087d0feadd867aa20f95bd7c3397e16
Kyle Stang <kylestang@users.noreply.github.com>
2022-11-27 15:18:43 -0800

    Works but slow

---
+++ ai/rust/src/main.rs
@@ -0,0 +1,45 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+
+mod state;
+
+#[derive(Deserialize, Clone, Debug)]
+struct MoveRequest {
+    vline: Vec<Vec<u8>>,
+    hline: Vec<Vec<u8>>,
+}
+
+#[derive(Serialize)]
+struct MoveResponse {
+    r#type: String,
+    location: [usize; 2],
+}
+
+#[get("/")]
+async fn test() -> impl Responder {
+    HttpResponse::Ok().body("It works!")
+}
+
+#[post("/move")]
+async fn next_move(req_body: web::Json<MoveRequest>) -> impl Responder {
+    println!("{:#?}", req_body);
+    let score = state::State::start_minimax(req_body.clone().hline, req_body.into_inner().vline);
+    let response = MoveResponse {
+        r#type: match score.vertical {
+            true => String::from("v"),
+            false => String::from("h"),
+        },
+        location: [score.row, score.column]
+    };
+    HttpResponse::Ok().json(response)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new()
+    .service(next_move)
+    .service(test))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
---
+++ ai/rust/src/state.rs
@@ -0,0 +1,232 @@
+#[derive(Clone)]
+pub struct State {
+    h_remaining: u32,
+    v_remaining: u32,
+    h_lines: Vec<Vec<u8>>,
+    v_lines: Vec<Vec<u8>>,
+    p1_turn: bool,
+    score: Score,
+    depth: u32
+}
+
+#[derive(Clone, Copy)]
+pub struct Score {
+    pub value: i32,
+    pub row: usize,
+    pub column: usize,
+    pub vertical: bool,
+}
+
+impl State {
+    pub fn start_minimax(h_lines: Vec<Vec<u8>>, v_lines: Vec<Vec<u8>>) -> Score {
+        let mut h_remaining = 0;
+        for column in &h_lines {
+            for &line in column {
+                if line == 0 {
+                    h_remaining += 1;
+                }
+            }
+        }
+
+        let mut v_remaining = 0;
+        for column in &v_lines {
+            for &line in column {
+                if line == 0 {
+                    v_remaining += 1;
+                }
+            }
+        }
+
+        let start_state = State {
+            h_remaining,
+            v_remaining,
+            h_lines,
+            v_lines,
+            p1_turn: true,
+            score: Score {
+                value: 0,
+                row: 0,
+                column: 0,
+                vertical: false,
+            },
+            depth: 0
+        };
+
+        start_state.minimax()
+    }
+
+    fn minimax(self) -> Score {
+        if self.depth >= 9 || self.h_remaining + self.v_remaining == 0 {
+            return self.score;
+        }
+
+        // maximising player
+        if self.p1_turn {
+            let mut new_max_score = Score {
+                value: i32::MIN,
+                row: 0,
+                column: 0,
+                vertical: false,
+            };
+            // vertical lines
+            for row in 0..self.v_lines.len() {
+                for column in 0..self.v_lines[0].len() {
+                    if self.v_lines[row][column] == 0 {
+                        let new_state = self.game_step_vertical(row, column);
+                        let new_score = new_state.minimax();
+                        if new_score.value > new_max_score.value {
+                            new_max_score.value = new_score.value;
+                            new_max_score.row = row;
+                            new_max_score.column = column;
+                            new_max_score.vertical = true;
+                        }
+                    }
+                }
+            }
+
+            // horizontal lines
+            for row in 0..self.h_lines.len() {
+                for column in 0..self.h_lines[0].len() {
+                    // Check if line is available
+                    if self.h_lines[row][column] == 0 {
+                        let new_state = self.game_step_horizontal(row, column);
+                        let new_score = new_state.minimax();
+                        if new_score.value > new_max_score.value {
+                            new_max_score.value = new_score.value;
+                            new_max_score.row = row;
+                            new_max_score.column = column;
+                            new_max_score.vertical = false;
+                        }
+                    }
+                }
+            }
+
+            new_max_score
+        } else {
+            // minimizing player
+            let mut new_min_score = Score {
+                value: i32::MAX,
+                row: 0,
+                column: 0,
+                vertical: false,
+            };
+
+            // vertical lines
+            for row in 0..self.v_lines.len() {
+                for column in 0..self.v_lines[0].len() {
+                    
+                    if self.v_lines[row][column] == 0 {
+                        let new_state = self.game_step_vertical(row, column);
+                        let new_score = new_state.minimax();
+                        if new_score.value < new_min_score.value {
+                            new_min_score.value = new_score.value;
+                            new_min_score.row = row;
+                            new_min_score.column = column;
+                            new_min_score.vertical = true;
+                        }
+                    }
+                }
+            }
+
+            // horizontal lines
+            for row in 0..self.h_lines.len() {
+                for column in 0..self.h_lines[0].len() {
+                    // Check if line is available
+                    if self.h_lines[row][column] == 0 {
+                        let new_state = self.game_step_horizontal(row, column);
+                        let new_score = new_state.minimax();
+                        if new_score.value < new_min_score.value {
+                            new_min_score.value = new_score.value;
+                            new_min_score.row = row;
+                            new_min_score.column = column;
+                            new_min_score.vertical = false;
+                        }
+                    }
+                }
+            }
+
+            new_min_score
+        }
+    }
+
+    fn game_step_vertical(&self, row: usize, column: usize) -> State {
+        // Create new board
+        let mut new_state = self.clone();
+        new_state.depth += 1;
+        new_state.p1_turn = !self.p1_turn;
+
+        new_state.v_lines[row][column] = 1;
+        new_state.v_remaining -= 1;
+
+        // check for square left
+        if column != 0 // not left edge
+        && new_state.v_lines[row][column - 1] != 0 // Left line
+        && new_state.h_lines[row][column - 1] != 0 // Top line
+        && new_state.h_lines[row + 1][column - 1] != 0 // bottom line
+        {
+            // Bottom line
+            new_state.score.value += match self.p1_turn {
+                true => 1,
+                false => -1,
+            };
+            new_state.p1_turn = self.p1_turn;
+        }
+
+
+        // check for square right
+        if column < new_state.v_lines[0].len() - 1
+        && new_state.v_lines[row][column + 1] != 0 // right line
+        && new_state.h_lines[row][column] != 0 // top line
+        && new_state.h_lines[row + 1][column] != 0 // bottom line
+        {
+            // bottom line
+            new_state.score.value += match self.p1_turn {
+                true => 1,
+                false => -1,
+            };
+            new_state.p1_turn = self.p1_turn;
+        }
+
+        new_state
+    }
+
+    fn game_step_horizontal(&self, row: usize, column: usize) -> State {
+        // Create new board
+        let mut new_state = self.clone();
+        new_state.depth += 1;
+        new_state.p1_turn = !self.p1_turn;
+
+        new_state.h_lines[row][column] = 1;
+        new_state.h_remaining -= 1;
+
+        // check for square up
+        if row != 0 // not left edge
+        && self.h_lines[row - 1][column] != 0 // top line
+        && self.v_lines[row - 1][column] != 0 // left line
+        && self.v_lines[row - 1][column + 1] != 0 // right line
+        {
+            // right line
+            new_state.score.value += match self.p1_turn {
+                true => 1,
+                false => -1,
+            };
+            new_state.p1_turn = self.p1_turn;
+        }
+
+        // check for square down
+        if row < self.h_lines.len() - 1
+        && self.h_lines[row + 1][column] != 0 // bottom line
+        && self.v_lines[row][column] != 0 // left line
+        && self.v_lines[row][column + 1] != 0 // right line
+        {
+            // right line
+            new_state.score.value += match self.p1_turn {
+                true => 1,
+                false => -1,
+            };
+            new_state.p1_turn = self.p1_turn;
+        }
+
+        new_state
+    }
+}
