ec140b4f00083c964d97291800b73b31f3bb132a
Kyle Stang <kylestang@users.noreply.github.com>
2022-11-27 15:51:15 -0800

    alpha/beta

--- ai/rust/src/main.rs
+++ ai/rust/src/main.rs
@@ -29,16 +29,14 @@ async fn next_move(req_body: web::Json<MoveRequest>) -> impl Responder {
             true => String::from("v"),
             false => String::from("h"),
         },
-        location: [score.row, score.column]
+        location: [score.row, score.column],
     };
     HttpResponse::Ok().json(response)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()
-    .service(next_move)
-    .service(test))
+    HttpServer::new(|| App::new().service(next_move).service(test))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
--- ai/rust/src/state.rs
+++ ai/rust/src/state.rs
@@ -6,7 +6,9 @@ pub struct State {
     v_lines: Vec<Vec<u8>>,
     p1_turn: bool,
     score: Score,
-    depth: u32
+    depth: u32,
+    alpha: i32,
+    beta: i32,
 }
 
 #[derive(Clone, Copy)]
@@ -49,13 +51,15 @@ impl State {
                 column: 0,
                 vertical: false,
             },
-            depth: 0
+            depth: 0,
+            alpha: i32::MIN,
+            beta: i32::MAX,
         };
 
         start_state.minimax()
     }
 
-    fn minimax(self) -> Score {
+    fn minimax(mut self) -> Score {
         if self.depth >= 9 || self.h_remaining + self.v_remaining == 0 {
             return self.score;
         }
@@ -68,8 +72,10 @@ impl State {
                 column: 0,
                 vertical: false,
             };
+
+            let mut dominated = false;
             // vertical lines
-            for row in 0..self.v_lines.len() {
+            'outer: for row in 0..self.v_lines.len() {
                 for column in 0..self.v_lines[0].len() {
                     if self.v_lines[row][column] == 0 {
                         let new_state = self.game_step_vertical(row, column);
@@ -80,22 +86,33 @@ impl State {
                             new_max_score.column = column;
                             new_max_score.vertical = true;
                         }
+                        if new_max_score.value >= self.beta {
+                            dominated = true;
+                            break 'outer;
+                        }
+                        self.alpha = self.alpha.max(new_max_score.value);
                     }
                 }
             }
 
-            // horizontal lines
-            for row in 0..self.h_lines.len() {
-                for column in 0..self.h_lines[0].len() {
-                    // Check if line is available
-                    if self.h_lines[row][column] == 0 {
-                        let new_state = self.game_step_horizontal(row, column);
-                        let new_score = new_state.minimax();
-                        if new_score.value > new_max_score.value {
-                            new_max_score.value = new_score.value;
-                            new_max_score.row = row;
-                            new_max_score.column = column;
-                            new_max_score.vertical = false;
+            if !dominated {
+                // horizontal lines
+                'outer: for row in 0..self.h_lines.len() {
+                    for column in 0..self.h_lines[0].len() {
+                        // Check if line is available
+                        if self.h_lines[row][column] == 0 {
+                            let new_state = self.game_step_horizontal(row, column);
+                            let new_score = new_state.minimax();
+                            if new_score.value > new_max_score.value {
+                                new_max_score.value = new_score.value;
+                                new_max_score.row = row;
+                                new_max_score.column = column;
+                                new_max_score.vertical = false;
+                            }
+                            if new_max_score.value >= self.beta {
+                                break 'outer;
+                            }
+                            self.alpha = self.alpha.max(new_max_score.value);
                         }
                     }
                 }
@@ -111,10 +128,10 @@ impl State {
                 vertical: false,
             };
 
+            let mut dominated = false;
             // vertical lines
-            for row in 0..self.v_lines.len() {
+            'outer: for row in 0..self.v_lines.len() {
                 for column in 0..self.v_lines[0].len() {
-                    
                     if self.v_lines[row][column] == 0 {
                         let new_state = self.game_step_vertical(row, column);
                         let new_score = new_state.minimax();
@@ -124,22 +141,33 @@ impl State {
                             new_min_score.column = column;
                             new_min_score.vertical = true;
                         }
+                        if new_min_score.value <= self.alpha {
+                            dominated = true;
+                            break 'outer;
+                        }
+                        self.beta = self.beta.min(new_min_score.value);
                     }
                 }
             }
 
             // horizontal lines
-            for row in 0..self.h_lines.len() {
-                for column in 0..self.h_lines[0].len() {
-                    // Check if line is available
-                    if self.h_lines[row][column] == 0 {
-                        let new_state = self.game_step_horizontal(row, column);
-                        let new_score = new_state.minimax();
-                        if new_score.value < new_min_score.value {
-                            new_min_score.value = new_score.value;
-                            new_min_score.row = row;
-                            new_min_score.column = column;
-                            new_min_score.vertical = false;
+            if !dominated {
+                'outer: for row in 0..self.h_lines.len() {
+                    for column in 0..self.h_lines[0].len() {
+                        // Check if line is available
+                        if self.h_lines[row][column] == 0 {
+                            let new_state = self.game_step_horizontal(row, column);
+                            let new_score = new_state.minimax();
+                            if new_score.value < new_min_score.value {
+                                new_min_score.value = new_score.value;
+                                new_min_score.row = row;
+                                new_min_score.column = column;
+                                new_min_score.vertical = false;
+                            }
+                            if new_min_score.value <= self.alpha {
+                                break 'outer;
+                            }
+                            self.beta = self.beta.min(new_min_score.value);
                         }
                     }
                 }
@@ -162,7 +190,8 @@ impl State {
         if column != 0 // not left edge
         && new_state.v_lines[row][column - 1] != 0 // Left line
         && new_state.h_lines[row][column - 1] != 0 // Top line
-        && new_state.h_lines[row + 1][column - 1] != 0 // bottom line
+        && new_state.h_lines[row + 1][column - 1] != 0
+        // bottom line
         {
             // Bottom line
             new_state.score.value += match self.p1_turn {
@@ -172,12 +201,12 @@ impl State {
             new_state.p1_turn = self.p1_turn;
         }
 
-
         // check for square right
         if column < new_state.v_lines[0].len() - 1
         && new_state.v_lines[row][column + 1] != 0 // right line
         && new_state.h_lines[row][column] != 0 // top line
-        && new_state.h_lines[row + 1][column] != 0 // bottom line
+        && new_state.h_lines[row + 1][column] != 0
+        // bottom line
         {
             // bottom line
             new_state.score.value += match self.p1_turn {
@@ -203,7 +232,8 @@ impl State {
         if row != 0 // not left edge
         && self.h_lines[row - 1][column] != 0 // top line
         && self.v_lines[row - 1][column] != 0 // left line
-        && self.v_lines[row - 1][column + 1] != 0 // right line
+        && self.v_lines[row - 1][column + 1] != 0
+        // right line
         {
             // right line
             new_state.score.value += match self.p1_turn {
@@ -217,7 +247,8 @@ impl State {
         if row < self.h_lines.len() - 1
         && self.h_lines[row + 1][column] != 0 // bottom line
         && self.v_lines[row][column] != 0 // left line
-        && self.v_lines[row][column + 1] != 0 // right line
+        && self.v_lines[row][column + 1] != 0
+        // right line
         {
             // right line
             new_state.score.value += match self.p1_turn {
